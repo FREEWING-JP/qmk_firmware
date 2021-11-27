9a2169ed19dc60f55d69b035b2da623788ec584c
FREEWING-JP <freewing.jp@gmail.com>
2021-11-27 18:46:42 +0900

    enable VIA Keymap function

--- keyboards/handwired/freewing/test_qmk/rules.mk
+++ keyboards/handwired/freewing/test_qmk/rules.mk
@@ -33,5 +33,7 @@ OLED_DRIVER = SSD1306       # OLED SSD1306 128x32
 
 DYNAMIC_MACRO_ENABLE = yes  # Dynamic Macro
 
+VIA_ENABLE = yes            # VIA Keymap function
+
 # LINK_TIME_OPTIMIZATION_ENABLE = yes
 LTO_ENABLE = yes # Config Option Link Time Optimization
